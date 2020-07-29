f43bd3af06ac383d51ae7e2eb07c5ad893e404d3
Fasjeit <15075638+Fasjeit@users.noreply.github.com>
2020-07-29 12:56:53 +0300

    Correct error in SetCspPrivateKey with unknown algorithm

--- src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Windows/CertificatePal.PrivateKey.cs
+++ src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Windows/CertificatePal.PrivateKey.cs
@@ -237,7 +237,7 @@ namespace Internal.Cryptography.Pal
                 }
                 default:
                 {
-                    return;
+                    throw new PlatformNotSupportedException();
                 }
             }
 

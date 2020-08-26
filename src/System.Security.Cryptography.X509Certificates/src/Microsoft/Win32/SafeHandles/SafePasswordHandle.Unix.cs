c1bba181051c44fbd5654f3fdaf0af2fdcff5c89
Anatoly Belyaev <tolya.msu@gmail.com>
2020-08-26 15:02:40 +0300

    Fix string to pwsz buffer conversion for Unix when import certificate from PFX

--- src/System.Security.Cryptography.X509Certificates/src/Microsoft/Win32/SafeHandles/SafePasswordHandle.Unix.cs
+++ src/System.Security.Cryptography.X509Certificates/src/Microsoft/Win32/SafeHandles/SafePasswordHandle.Unix.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Security;
+using System.Text;
 
 namespace Microsoft.Win32.SafeHandles
 {
@@ -12,7 +13,7 @@ namespace Microsoft.Win32.SafeHandles
     {
         private IntPtr CreateHandle(string password)
         {
-            return Marshal.StringToHGlobalAnsi(password);
+            return StringToHGlobalUTF32(password);
         }
 
         private IntPtr CreateHandle(SecureString password)
@@ -22,7 +23,45 @@ namespace Microsoft.Win32.SafeHandles
 
         private void FreeHandle()
         {
-            Marshal.ZeroFreeGlobalAllocAnsi(handle);
+            Marshal.FreeHGlobal(s);
+            // Marshal.ZeroFreeGlobalAllocAnsi(handle);
+        }
+
+        //Copy from Masrshal package with modification to support UTF32
+        private static unsafe IntPtr StringToHGlobalUTF32(string s)
+        {
+            int size_of_wchar_t = 4;
+            if (s is null)
+            {
+                return IntPtr.Zero;
+            }
+
+            long lnb = (s.Length + 1) * (long)size_of_wchar_t;
+            int nb = (int)lnb;
+
+            // Overflow checking
+            if (nb != lnb)
+            {
+                throw new ArgumentOutOfRangeException(nameof(s));
+            }
+
+            IntPtr hglobal = Marshal.   AllocHGlobal((IntPtr)nb);
+
+            StringToUTF32String(s, (byte*)hglobal, nb);
+            return hglobal;
+        }
+
+        private static unsafe int StringToUTF32String(string s, byte* buffer, int bufferLength, bool bestFit = false, bool throwOnUnmappableChar = false)
+        {
+            int convertedBytes;
+            fixed (char* pChar = s)
+            {
+                convertedBytes = Encoding.UTF32.GetBytes(pChar, s.Length, buffer, bufferLength);
+            }
+
+            buffer[convertedBytes] = 0;
+
+            return convertedBytes;
         }
     }
 }
